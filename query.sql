7c4c33f8dd36e8d083aba34349bb7bded0aaad8f
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-01 17:04:26 +0100

    varius fix

--- query.sql
+++ query.sql
@@ -1,7 +1,6 @@
 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT * FROM `students` WHERE `date_of_birth` LIKE '1990-%';
-
+SELECT * FROM `students` WHERE YEAR (`date_of_birth`) = '1990';
 
 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
@@ -10,7 +9,8 @@ SELECT * FROM `courses` WHERE `cfu` > '10';
 
 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
-SELECT * FROM students WHERE `date_of_birth` < '1993';
+SELECT * FROM students WHERE TIMESTAMPDIFF(MONTH,`date_of_birth`,'1991-01-01');
+
 
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
