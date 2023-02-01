bbf9ec7f1771379d89b3fe6ff16c0d328e45d10d
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-01 17:14:49 +0100

    changed query in 1) & 3)

--- query.sql
+++ query.sql
@@ -1,6 +1,7 @@
 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT * FROM `students` WHERE YEAR (`date_of_birth`) = '1990';
+SELECT * FROM `students` WHERE YEAR (`date_of_birth`) = 1990;
+
 
 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
@@ -9,8 +10,7 @@ SELECT * FROM `courses` WHERE `cfu` > '10';
 
 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
-SELECT * FROM students WHERE TIMESTAMPDIFF(MONTH,`date_of_birth`,'1991-01-01');
-
+SELECT * FROM students WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > 30; 
 
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
