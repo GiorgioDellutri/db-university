ebe7521c5dd4e6d4ca5437512eaef34c6a92a1a2
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-02 19:00:37 +0100

    group 3 & 4

--- group.sql
+++ group.sql
@@ -4,8 +4,20 @@ SELECT COUNT(*) AS 'Numero di iscritti', YEAR(`enrolment_date`) AS 'Anno'
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
 
-2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+2. Contare gli insegnanti che hanno l ufficio nello stesso edificio
 
 SELECT COUNT(*) AS 'Numero insegnanti',  `office_address` AS 'Indirizzo ufficio'
 FROM `teachers`
 GROUP BY `office_address`; 
+
+3. Calcolare la media dei voti di ogni appello d esame
+
+SELECT `exam_id` AS 'Id esame', AVG(`vote`) AS 'Voto medio'
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT  `department_id` AS 'ID Dipartimento' ,COUNT(*) AS 'Numero Corsi di Laurea'
+FROM `degrees`
+GROUP BY `department_id`;
